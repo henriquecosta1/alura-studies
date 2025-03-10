0ff3f8230583dc541dd2258f4808f393aa490f6e
Henrique Costa <contato.henrique19@gmail.com>
2025-03-10 20:50:27 -0300

    selecao tarefas e tempo cronometro

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,13 +1,27 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from "./Cronometro.module.scss";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
+import { tempoParaSegundos } from "../../common/utils/tempo";
 
-export default function Cronometro() {
+interface Props {
+    selecionado: ITarefa | undefined
+}
+
+export default function Cronometro({selecionado} : Props) {
+    const [tempo, setTempo] = useState<number>();
+    useEffect(() => {
+        if(selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado.tempo));
+        }
+    }, [selecionado]);
+
     return (
         <div className={style.cronometro}>
             <p className={style.titulo} >Escolha um card e inicie o cronômetro</p>
             <div className={style.relogioWrapper}>
-                <Relogio />
+                <Relogio tempo={tempo} />
             </div>
             <Botao>
                 Começar
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -2,13 +2,20 @@ import style from './Lista.module.scss'
 import Item from "./Item";
 import { ITarefa } from '../../types/tarefa';
 
-function Lista({tarefas} : {tarefas: ITarefa[]}) {
+interface Props {
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+function Lista({tarefas, selecionaTarefa} : Props) {
     return (
         <aside className={style.listaTarefas}>
             <h2> Estudos do dia </h2>
             <ul>
-                {tarefas.map((item, index) => (
-                    <Item key={index}
+                {tarefas.map((item) => (
+                    <Item 
+                        selecionaTarefa={selecionaTarefa}
+                        key={item.id}
                         {...item}
                     />
                 ))}
